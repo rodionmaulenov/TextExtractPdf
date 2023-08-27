3e6e0b47d70523bb7e48aba86b9ca51189c43fa3
rodionmaulenov <natalia.maulenova@gmail.com>
2023-08-28 00:53:36 +0300

    new commit

--- work_dir/home_api/settings.py
+++ work_dir/home_api/settings.py
@@ -21,14 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECRET_KEY = 'django-insecure-fy!otfu*^lbwn_%vav4rw+d$8%4afj4j5rme&$(&eh$8jh4nea'
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
 # DEBUG = True
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 # ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
