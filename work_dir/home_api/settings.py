0a9cfbfc04f4fa753bec79f9c1a39a0e7a090687
rodionmaulenov <natalia.maulenova@gmail.com>
2023-08-29 02:06:09 +0300

    new_commit

--- work_dir/home_api/settings.py
+++ work_dir/home_api/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'home_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': "django.db.backends.postgresql_psycopg2",
+        'ENGINE': "django.db.backends.postgresql",
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
