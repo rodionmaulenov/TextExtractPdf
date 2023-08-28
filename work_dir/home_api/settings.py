ba1f20b83bdded0fb9587b29f1e2d7dc093edf1a
rodionmaulenov <natalia.maulenova@gmail.com>
2023-08-29 02:45:34 +0300

    new_commit

--- work_dir/home_api/settings.py
+++ work_dir/home_api/settings.py
@@ -83,28 +83,28 @@ WSGI_APPLICATION = 'home_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': "django.db.backends.postgresql_psycopg2",
-#         'NAME': os.environ.get('DB_NAME'),
-#         'USER': os.environ.get('DB_USER'),
-#         'PASSWORD': os.environ.get('DB_PASSWORD'),
-#         'HOST': os.environ.get('DB_HOST'),
-#         'PORT': os.environ.get('DB_PORT'),
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'yourdbname',
-        'USER': 'yourdbuser',
-        'PASSWORD': 'yourdbpassword',
-        'HOST': 'localhost',  # or '127.0.0.1'
-        'PORT': '5432',
+        'ENGINE': "django.db.backends.postgresql_psycopg2",
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'yourdbname',
+#         'USER': 'yourdbuser',
+#         'PASSWORD': 'yourdbpassword',
+#         'HOST': 'localhost',  # or '127.0.0.1'
+#         'PORT': '5432',
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
@@ -116,6 +116,7 @@ DATABASES = {
 #         'PORT': '5432',
 #     }
 # }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
