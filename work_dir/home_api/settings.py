9f03293ecdfdcedeebdc6a40caefed22a5232423
rodionmaulenov <natalia.maulenova@gmail.com>
2023-08-28 17:07:53 +0300

    new docker commit

--- work_dir/home_api/settings.py
+++ work_dir/home_api/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'home_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('DB_ENGINE'),
+        'ENGINE': "django.db.backends.postgresql_psycopg2",
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
