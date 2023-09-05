5e17dd7d5c86682b30ab7026c8a201aad448d465
rodionmaulenov <natalia.maulenova@gmail.com>
2023-09-05 23:13:54 +0300

    new commit

--- work_dir/home_api/settings.py
+++ work_dir/home_api/settings.py
@@ -12,11 +12,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
-SECRET_KEY = 'django-insecure-fy!otfu*^lbwn_%vav4rw+d$8%4afj4j5rme&$(&eh$8jh4nea'
-
-DEBUG = True
-
-ALLOWED_HOSTS = ['127.0.0.1']
+# SECRET_KEY = 'django-insecure-fy!otfu*^lbwn_%vav4rw+d$8%4afj4j5rme&$(&eh$8jh4nea'
+#
+# DEBUG = True
+#
+# ALLOWED_HOSTS = ['127.0.0.1']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,13 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-#
-# # SECURITY WARNING: don't run with debug turned on in production!
-#
-# DEBUG = os.environ.get('DEBUG')
-#
-# ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+
+DEBUG = os.environ.get('DEBUG')
+
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 
 # Application definition
 
