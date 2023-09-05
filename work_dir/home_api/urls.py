5e1234444b924a466b8a5cb20ced577e3f916272
rodionmaulenov <natalia.maulenova@gmail.com>
2023-09-05 23:06:22 +0300

    new commit

--- work_dir/home_api/urls.py
+++ work_dir/home_api/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("__debug__/", include("debug_toolbar.urls")),
+    # path("__debug__/", include("debug_toolbar.urls")),
 ]
 
 if settings.DEBUG:
