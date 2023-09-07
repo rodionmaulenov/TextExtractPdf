1d5b699e454a6c5674672cc6796a838b236e6b9b
rodionmaulenov <natalia.maulenova@gmail.com>
2023-09-07 15:40:54 +0300

    new commit

--- work_dir/home_api/settings.py
+++ work_dir/home_api/settings.py
@@ -23,6 +23,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
+print(DEBUG)
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'), os.environ.get('SQL_HOST')]
 
