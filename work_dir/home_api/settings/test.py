15c8122c1e2f2ffe5fc6ce19d72b1a33524b09c1
rodionmaulenov <natalia.maulenova@gmail.com>
2023-09-08 22:57:44 +0300

    new commit

---
+++ work_dir/home_api/settings/test.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': "django.db.backends.postgresql_psycopg2",
+        'NAME': os.environ.get('SQL_NAME'),
+        'USER': os.environ.get('SQL_USER'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD'),
+        'HOST': os.environ.get('SQL_HOST'),
+        'PORT': os.environ.get('SQL_PORT'),
+    }
+}
