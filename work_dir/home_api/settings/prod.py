8177d6469f29356b30952a84e4da9cf720c477a8
rodionmaulenov <natalia.maulenova@gmail.com>
2023-09-11 18:53:49 +0300

    new test

--- work_dir/home_api/settings/prod.py
+++ work_dir/home_api/settings/prod.py
@@ -28,17 +28,18 @@ CSRF_COOKIE_SECURE = True
 CSRF_TRUSTED_ORIGINS = [os.environ.get('CSRF_TRUSTED_ORIGINS')]
 
 
-# AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-# AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-# AWS_STORAGE_BUCKET_NAME = 'text-extract-pdf-spaces'
-# AWS_S3_ENDPOINT_URL = "https://fra1.digitaloceanspaces.com"
-# AWS_S3_OBJECT_PARAMETERS = {
-#     "CacheControl": "max-age=86400",
-#     "ACL": "public-read"
-# }
-# AWS_LOCATION = "https://text-extract-pdf-spaces.fra1.digitaloceanspaces.com"
-# DEFAULT_FILE_STORAGE = "home_api.cdn.backends.MediaRootS3BotoStorage"
-# STATICFILES_STORAGE = 'home_api.cdn.backends.StaticRootS3BotoStorage'
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = 'text-extract-pdf-spaces'
+AWS_S3_ENDPOINT_URL = "https://fra1.digitaloceanspaces.com"
+AWS_S3_OBJECT_PARAMETERS = {
+    "CacheControl": "max-age=86400",
+    "ACL": "public-read"
+}
+MEDIA_URL = "https://text-extract-pdf-spaces.fra1.digitaloceanspaces.com/media/"
+STATIC_URL = "https://text-extract-pdf-spaces.fra1.digitaloceanspaces.com/static/"
+DEFAULT_FILE_STORAGE = "home_api.cdn.backends.MediaRootS3BotoStorage"
+STATICFILES_STORAGE = 'home_api.cdn.backends.StaticRootS3BotoStorage'
 
 
 
