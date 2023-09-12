31e1a736e93d6ffffb0e19083a081cdf9f85a529
rodionmaulenov <natalia.maulenova@gmail.com>
2023-09-12 19:25:26 +0300

    new test

--- work_dir/home_api/settings/prod.py
+++ work_dir/home_api/settings/prod.py
@@ -24,22 +24,3 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
 CSRF_TRUSTED_ORIGINS = [os.environ.get('CSRF_TRUSTED_ORIGINS')]
-
-# S3 Bucket DigitalOcean
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = 'text-extract-pdf-spaces'
-AWS_S3_ENDPOINT_URL = "https://fra1.digitaloceanspaces.com"
-AWS_DEFAULT_ACL = 'public-read'
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400'
-}
-
-AWS_STATIC_LOCATION = 'static'
-STATIC_URL = '%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_STATIC_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-AWS_MEDIA_LOCATION = 'media'
-PUBLIC_MEDIA_LOCATION = 'media'
-MEDIA_URL = '%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_MEDIA_LOCATION)
-DEFAULT_FILE_STORAGE = 'home_api.storage_backends.MediaStorage'
